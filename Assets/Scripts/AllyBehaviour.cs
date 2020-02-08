875398ae4632d933b3f09fa23179360f0058b425
Wingedevil <jerroldtan@gmail.com>
2020-02-08 19:00:47 +0800

    Initial pathfinding commit

--- Assets/Scripts/AllyBehaviour.cs
+++ Assets/Scripts/AllyBehaviour.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections;
+﻿using Pathfinding;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AllyBehaviour : MonoBehaviour
-{
-    UnityEngine.AI.NavMeshAgent agent;
+public class AllyBehaviour : MonoBehaviour {
+    public GameObject TargetPrefab;
 
-    public void Start() {
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    AIDestinationSetter _destinationSetterReference;
+    GameObject _targetReference;
+    // Start is called before the first frame update
+    void Start() {
+        _targetReference = Instantiate(TargetPrefab, transform.position, Quaternion.identity);
+        _destinationSetterReference = GetComponentInParent<AIDestinationSetter>();
+        _destinationSetterReference.target = _targetReference.transform;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
     }
 
     public void Move(RaycastHit hit) {
-        agent.SetDestination(hit.point);
+        Debug.Log("Target = " + hit.point);
+        _targetReference.transform.position = hit.point;
     }
 }
---
+++ Assets/Scripts/MovingAndBehavior/BasicBehavior.cs
@@ -0,0 +1,29 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBehavior : MonoBehaviour
+{
+    public GameObject TargetPrefab;
+
+    Seeker _seekerReference;
+    GameObject _targetReference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _targetReference = Instantiate(TargetPrefab);
+        _seekerReference = GetComponent<Seeker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Move(RaycastHit hit) {
+        Debug.Log("Potato");
+        _seekerReference.transform.position = hit.point;
+    }
+}
