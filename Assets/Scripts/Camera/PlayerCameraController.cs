f7cdba2f071576fb3a8194d77c82f6dc0bd1929d
limchekjun <limchekjun@gmail.com>
2020-02-20 18:22:21 +0800

    Adjusted camera so the border in Z direction is same for both zoomed in and zoomed out scenarios.

--- Assets/Scripts/Camera/PlayerCameraController.cs
+++ Assets/Scripts/Camera/PlayerCameraController.cs
@@ -9,39 +9,39 @@ public class PlayerCameraController : MonoBehaviour
 
     public float scrollSpeed = 20f;
     public float minY = 20f;
-    public float maxY = 120f;
+    public float maxY = 60f;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
 
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        if (Input.GetKey("up") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             pos.z += panSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        if (Input.GetKey("left") || Input.mousePosition.x <= panBorderThickness)
         {
             pos.x -= panSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        if (Input.GetKey("down") || Input.mousePosition.y <= panBorderThickness)
         {
             pos.z -= panSpeed * Time.deltaTime;
         }
 
-        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        if (Input.GetKey("right") || Input.mousePosition.x >= Screen.width - panBorderThickness)
         {
             pos.x += panSpeed * Time.deltaTime;
         }
 
         float scroll = Input.GetAxis("Mouse ScrollWheel");
         pos.y -= scroll * scrollSpeed * 100f * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
         pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y - ((pos.y - minY) / Mathf.Sqrt(3f)), panLimit.y - ((pos.y - minY) / Mathf.Sqrt(3f)));
 
         transform.position = pos;
     }
