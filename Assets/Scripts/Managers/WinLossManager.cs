ddfb5baac11b8ece81af9c162b64211ec5b7a531
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2020-02-22 11:23:04 +0800

    Added win and loss canvases

--- Assets/Scripts/Managers/WinLossManager.cs
+++ Assets/Scripts/Managers/WinLossManager.cs
@@ -6,6 +6,11 @@ public class WinLossManager : MonoBehaviour
 {
     UiOverlayManager _uiOverlayManager;
 
+    GameObject winScreenCanvas;
+    GameObject loseScreenCanvas;
+    LevelStatus levelStatus;
+
+
     enum LevelStatus
     {
         Ongoing,
@@ -13,13 +18,26 @@ public class WinLossManager : MonoBehaviour
         Lose
     }
 
+    void Start()
+    {
+        levelStatus = LevelStatus.Ongoing;
+        
+        winScreenCanvas = GameObject.Find("WinScreenCanvas");
+        winScreenCanvas.SetActive(false);
+
+        loseScreenCanvas = GameObject.Find("LoseScreenCanvas");
+        loseScreenCanvas.SetActive(false);
+    }
+
     public void ReportTruckReachedLevelEnd()
     {
-        
+        levelStatus = LevelStatus.Win;
+        winScreenCanvas.SetActive(true);
     }
 
     public void ReportTruckDead()
     {
-        
+        levelStatus = LevelStatus.Lose;
+        loseScreenCanvas.SetActive(true);
     }
 }
