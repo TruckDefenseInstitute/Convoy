28b94d75956dc7852a9a39a673293809f8659564
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2020-02-22 09:36:40 +0800

    Added Pause Screen with buttons that have no functionality

---
+++ Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public KeyCode pauseButton;
+
+    GameObject pauseScreenCanvas;
+    bool gamePaused = false;
+    float originalTimeScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseScreenCanvas = GameObject.Find("PauseScreenCanvas");
+        pauseScreenCanvas.SetActive(false);
+        originalTimeScale = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseButton))
+        {
+            if (gamePaused)
+            {
+                UnpauseGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+
+            gamePaused = !gamePaused;
+        }
+    }
+
+    void PauseGame()
+    {
+        pauseScreenCanvas.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    void UnpauseGame()
+    {
+        pauseScreenCanvas.SetActive(false);
+        Time.timeScale = originalTimeScale;
+    }
+}
---
+++ Assets/Scripts/Managers/WinLossManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLossManager : MonoBehaviour
+{
+    UiOverlayManager _uiOverlayManager;
+
+    enum LevelStatus
+    {
+        Ongoing,
+        Win,
+        Lose
+    }
+
+    public void ReportTruckReachedLevelEnd()
+    {
+        
+    }
+
+    public void ReportTruckDead()
+    {
+        
+    }
+}
