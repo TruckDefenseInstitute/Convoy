aee70450712b7f8cd2796613848c276fd87688ca
Kyzure <Kyzurene@gmail.com>
2020-02-25 16:24:14 +0800

    Fixed Deployment Bug

--- Assets/Scripts/Ui/DeployedUnitDictionary.cs
+++ Assets/Scripts/Ui/DeployedUnitDictionary.cs
@@ -14,14 +14,18 @@ public class DeployedUnitDictionary : MonoBehaviour {
         switch(unit.Name) {
             case "Attack Bike": 
                 return _unitDeployedButtonPrefab[0];
-            case "Grunt": 
+            case "Grenadier": 
                 return _unitDeployedButtonPrefab[1];
-            case "Rifleman": 
+            case "Grunt": 
                 return _unitDeployedButtonPrefab[2];
-            case "Sniper": 
+            case "Rifleman": 
                 return _unitDeployedButtonPrefab[3];
-            case "ZoneTrooper": 
+            case "Sniper": 
                 return _unitDeployedButtonPrefab[4];
+            case "Tank":
+                return _unitDeployedButtonPrefab[5];
+            case "ZoneTrooper":
+                return _unitDeployedButtonPrefab[6];
             default:
                 return _unitDeployedButtonPrefab[0]; // Backup
         }
@@ -31,14 +35,18 @@ public class DeployedUnitDictionary : MonoBehaviour {
         switch(unit.Name) {
             case "Attack Bike": 
                 return _unitDeployedButtonMultiplePrefab[0];
-            case "Grunt": 
+            case "Grenadier": 
                 return _unitDeployedButtonMultiplePrefab[1];
-            case "Rifleman": 
+            case "Grunt": 
                 return _unitDeployedButtonMultiplePrefab[2];
-            case "Sniper": 
+            case "Rifleman": 
                 return _unitDeployedButtonMultiplePrefab[3];
-            case "ZoneTrooper": 
+            case "Sniper": 
                 return _unitDeployedButtonMultiplePrefab[4];
+            case "Tank":
+                return _unitDeployedButtonMultiplePrefab[5];
+            case "ZoneTrooper":
+                return _unitDeployedButtonMultiplePrefab[6];
             default:
                 return _unitDeployedButtonMultiplePrefab[0]; // Backup
         }
