5667bfb5d3cb778bfb335d34afa4ca69206f8466
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2020-02-25 19:00:26 +0800

    - Created summoning circle for when units are summoned - Decreased all summon timing by 0.5 to account for the 0.5 second delay during summoning (so that it looks like the unit is getting summoned by the circle)

---
+++ Assets/Scripts/SelfdestructInTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfdestructInTime : MonoBehaviour
+{
+    public float timeToDestroy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("Destroy", timeToDestroy);
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
