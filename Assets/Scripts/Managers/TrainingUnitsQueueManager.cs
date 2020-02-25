e5d52b79edd86ec25b9cc6842d28713e9426c4ec
Wingedevil <jerroldtan@gmail.com>
2020-02-25 20:07:59 +0800

    fix: fix summoning radius

--- Assets/Scripts/Managers/TrainingUnitsQueueManager.cs
+++ Assets/Scripts/Managers/TrainingUnitsQueueManager.cs
@@ -54,7 +54,7 @@ public class TrainingUnitsQueueManager : Manager<TrainingUnitsQueueManager> {
 
     private IEnumerator DeployUnitCoroutine(GameObject unit) {
         Vector3 currentTruckPosition = TruckReferenceManager.Instance.TruckGameObject.transform.position;
-        Vector3 divergence = Random.insideUnitCircle * TruckReferenceManager.Instance.TruckBehavior.GetComponent<RichAI>().radius * 2f;
+        Vector3 divergence = Random.insideUnitCircle.normalized * TruckReferenceManager.Instance.TruckBehavior.GetComponent<RichAI>().radius * 2f;
         divergence.z = divergence.y;
         divergence.y = 0;
         Instantiate(_summonCircle, currentTruckPosition + divergence + new Vector3(0, 1, 0), TruckReferenceManager.Instance.TruckGameObject.transform.rotation);
