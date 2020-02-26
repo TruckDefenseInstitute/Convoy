4352f4f45c4168908854a41fd172647c12e9f26f
marcusteh1238 <marcusteh1238@gmail.com>
2020-02-26 16:54:31 +0800

    Add cursor Asset

---
+++ Assets/Cursor/mouseCursor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseCursor : MonoBehaviour
+{
+
+    public SpriteRenderer rend;
+    public Sprite normalCursor;
+    public Sprite mouseDownCursor;
+
+    void Start() 
+    {
+        // Cursor.visible = false;
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        transform.position = cursorPos;
+        Debug.Log(cursorPos);
+        if (Input.GetMouseButtonDown(0))
+        {
+            rend.sprite = mouseDownCursor;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            rend.sprite = normalCursor;
+        }
+    }
+}
