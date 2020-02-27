c82cc94f3d22401b9414834e54a3863072bf6fda
Wingedevil <jerroldtan@gmail.com>
2020-02-27 17:49:16 +0800

    feat: added smartass AI
    AI now forgets about your tanky ass truck in favor of killing your little, little fragile infantry

--- Assets/Scripts/Projectiles/RocketVolleyProjectile.cs
+++ Assets/Scripts/Projectiles/RocketVolleyProjectile.cs
@@ -18,6 +18,7 @@ public class RocketVolleyProjectile : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
+        DamageMetadata = new DamageMetadata(DamageMetadata.Damage / NumberOfRockets, DamageMetadata.DamageType);
         FireRocket();
     }
 
