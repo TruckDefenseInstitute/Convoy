90d040ea03921126086341e327bc05c6b751d0d9
Wingedevil <jerroldtan@gmail.com>
2020-03-17 01:24:28 +0800

    fix: fix minimap icons not being colored correctly
    Additional notes:
    fix: fix some minimpa textures being rendered without interpolation
    refactor: refactor minimap icon to be recolorable
    feat: add variable for minimap icon size

--- Assets/Scripts/Units/UnitAlignmentIndicator.cs
+++ Assets/Scripts/Units/UnitAlignmentIndicator.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 [RequireComponent(typeof(Unit))]
 public class UnitAlignmentIndicator : MonoBehaviour {
+    [Tooltip("Indicates how big to draw the minimap indicator. Scale multiplier grows asymptotically to square root of the Threat Level.")]
+    public float ThreatLevel = 1;
+    public GameObject AlliedMinimapIndicator;
+    public GameObject EnemyMinimapIndicator;
+
+    public Color AlliedColor = Color.blue;
+    public Color EnemyColor = Color.red;
 
     Unit _unit;
     GameObject _child;
@@ -15,7 +22,13 @@ public class UnitAlignmentIndicator : MonoBehaviour {
         _unit = GetComponent<Unit>();
         _child = new GameObject("Alignment Light");
         _light = _child.AddComponent<Light>();
-        _light.color = _unit.Alignment == Alignment.Friendly ? Color.blue : Color.red;
+        if (_unit.Alignment == Alignment.Friendly) {
+            _light.color = AlliedColor;
+            Instantiate(AlliedMinimapIndicator, transform).transform.localScale *= Mathf.Sqrt(ThreatLevel);
+        } else if (_unit.Alignment == Alignment.Hostile) {
+            _light.color = EnemyColor;
+            Instantiate(EnemyMinimapIndicator, transform).transform.localScale *= Mathf.Sqrt(ThreatLevel);
+        }
         if (TryGetComponent<RichAI>(out RichAI ai)) {
             _light.range = ai.radius * 4;
         } else if (TryGetComponent<NavmeshCut>(out NavmeshCut nc)) {
