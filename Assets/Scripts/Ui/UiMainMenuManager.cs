2b6cee968080e9113b52f3f31e50ee74788edb14
Kyzure <Kyzurene@gmail.com>
2020-03-26 20:55:56 +0800

    Added main menu without options

---
+++ Assets/Scripts/Ui/UiMainMenuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor.Animations;
+
+public class UiMainMenuManager : Manager<UiMainMenuManager> {
+
+    [SerializeField]
+    private List<Animator> _animatorList;
+
+    public void StartGame() {
+        UiSoundManager.Instance.PlayClickSound();
+        DisappearButtons();
+        StartCoroutine(LateStartGame());
+    }
+
+    public void Options() {
+        UiSoundManager.Instance.PlayClickSound();
+        StartCoroutine(LateOption());
+    }
+
+    public void Quit() {
+        UiSoundManager.Instance.PlayClickSound();
+        DisappearButtons();
+        StartCoroutine(LateQuit());
+    }
+
+    private void DisappearButtons() {
+        foreach(Animator animator in _animatorList) {
+            animator.SetBool("Disappeared", true);
+            animator.SetTrigger("Disappear");
+        }
+    }
+
+    IEnumerator LateStartGame() {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    IEnumerator LateOption() {
+        yield return new WaitForSeconds(1);
+        
+    }
+
+    IEnumerator LateQuit() {
+        yield return new WaitForSeconds(1);
+        Application.Quit();
+    }
+}
