caf9b6702d7b8ce63d59b4677b907f5873ec0237
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2020-03-27 22:05:33 +0800

    Made a Blockade class to handle stopping blockade reinforcements when the Blockade dies.

---
+++ Assets/Scripts/Units/Blockade.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blockade : Unit
+{
+    // Start is called before the first frame update
+    private new void Start() {
+        base.Start();
+
+        DeathCallback = () => {
+            
+            GetChildByName(GetChildByName(gameObject.transform.parent.gameObject, "BlockadeReinforcements"), "BlockadeReinforcementSpawns").GetComponent<EndBlockadeReinforcementsEvent>().Trigger();
+
+            /*
+            GameObject.Find("BlockadeReinforcementSpawns")
+                      .GetComponent<EndBlockadeReinforcementsEvent>()
+                      .Trigger();
+            */
+        };
+    }
+
+    new void Update() {
+        base.Update();
+    }
+
+    GameObject GetChildByName(GameObject parent, string name)
+    {
+        for (int i = 0; i < parent.transform.childCount; i++)
+        {
+            if (parent.transform.GetChild(i).gameObject.name == name)
+            {
+                return parent.transform.GetChild(i).gameObject;
+            }
+        }
+
+        return null;
+    }
+}
