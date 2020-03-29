87c6d4bd83c597ff9ee263a3c6dd35c354ba1908
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2020-03-29 15:07:27 +0800

    Increased the size of Popup boxes to make them easier to read. Added flavour text.

--- Assets/Scripts/Ui/UiPopUp.cs
+++ Assets/Scripts/Ui/UiPopUp.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using TMPro;
 
@@ -9,6 +10,13 @@ public class UiPopUp : MonoBehaviour {
     private TextMeshProUGUI _name = null;
     private TextMeshProUGUI _time = null;
     private TextMeshProUGUI _description = null;
+    private TextMeshProUGUI _flavour = null;
+
+    private RectTransform rect;
+
+    private float _boxWidth = 500;
+    private float _baseHeight = 100;
+    private float _heightPerLine = 30f;
 
     // Can't use start
     void Awake() {
@@ -16,30 +24,42 @@ public class UiPopUp : MonoBehaviour {
         _cost = transform.GetChild(1).GetChild(2).gameObject.GetComponent<TextMeshProUGUI>();
         _time = transform.GetChild(2).GetChild(2).gameObject.GetComponent<TextMeshProUGUI>();
         _description = transform.GetChild(3).gameObject.GetComponent<TextMeshProUGUI>();
+        _flavour = transform.GetChild(4).gameObject.GetComponent<TextMeshProUGUI>();
+
+        rect = GetComponent<RectTransform>();
+
+        _boxWidth = rect.sizeDelta.x;
+        _baseHeight = rect.sizeDelta.y;
     }
 
     public void Configure(GameObject unit, GameObject parent) {
-        RectTransform rect = GetComponent<RectTransform>();
         float minX = rect.offsetMin.x;
         float minY = rect.offsetMin.y;
         float maxX = rect.offsetMax.x;
         float maxY = rect.offsetMax.y;
         
         _name.text = unit.GetComponent<Unit>().Name;
-        _cost.text = unit.GetComponent<UnitTraining>().GetUnitCost().ToString("0");
-        _time.text = unit.GetComponent<UnitTraining>().GetUnitTrainingTime().ToString("0");
-        _description.text = unit.GetComponent<UnitTraining>().GetUnitDescription();
+        
+        UnitTraining ut = unit.GetComponent<UnitTraining>();
+        
+        _cost.text = ut.GetUnitCost().ToString("0");
+        _time.text = ut.GetUnitTrainingTime().ToString("0");
+        _description.text = ut.GetUnitDescription();
+        _flavour.text = ut.GetUnitFlavourText();
 
         transform.SetParent(parent.transform);
 
         rect.localScale = new Vector3(1, 1, 1);
         rect.offsetMin = new Vector2(minX, maxX);
         rect.offsetMax = new Vector2(minY, maxY);
-        rect.sizeDelta = new Vector2(300, 70 + GetStringLines(_description.text) * 20);
-        
+
+        float boxHeight = _baseHeight + GetStringLines(_description.text) + GetStringLines(_flavour.text) * _heightPerLine;
+        rect.sizeDelta = new Vector2(_boxWidth, boxHeight);
+        rect.localPosition = new Vector3(-_boxWidth / 2, boxHeight / 2);
     }
 
     private int GetStringLines(string str) {
-        return str.Length / 32;
+        Regex rx = new Regex("<br>");
+        return rx.Matches(str).Count + str.Length / 40;
     }
 }
