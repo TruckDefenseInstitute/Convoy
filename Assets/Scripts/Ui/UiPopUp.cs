bfb17b9ed00346056c19b7fa1872c7138140c324
ChengGeng97 <47176493+ChengGeng97@users.noreply.github.com>
2020-03-29 15:32:38 +0800

    Made the UI Popup start to fade after 2 seconds to account for the larger size.

--- Assets/Scripts/Ui/UiPopUp.cs
+++ Assets/Scripts/Ui/UiPopUp.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class UiPopUp : MonoBehaviour {
-    
+    // For fading
+    private float _timeSinceBirth;
+    [SerializeField]
+    private float _timeStartFade;
+    [SerializeField]
+    private float _fadeSpeed;
+    [SerializeField]
+    private float _alphaUpperBound;
+    private bool _hitUpperBound = false;
+
+    // For setting up the box properly
     private TextMeshProUGUI _cost = null;
     private TextMeshProUGUI _name = null;
     private TextMeshProUGUI _time = null;
     private TextMeshProUGUI _description = null;
     private TextMeshProUGUI _flavour = null;
 
-    private RectTransform rect;
+    private Image _blackSurfaceBox = null;
+
+    private RectTransform _rect;
 
     private float _boxWidth = 500;
     private float _baseHeight = 100;
@@ -26,17 +39,19 @@ public class UiPopUp : MonoBehaviour {
         _description = transform.GetChild(3).gameObject.GetComponent<TextMeshProUGUI>();
         _flavour = transform.GetChild(4).gameObject.GetComponent<TextMeshProUGUI>();
 
-        rect = GetComponent<RectTransform>();
+        _rect = GetComponent<RectTransform>();
 
-        _boxWidth = rect.sizeDelta.x;
-        _baseHeight = rect.sizeDelta.y;
+        _boxWidth = _rect.sizeDelta.x;
+        _baseHeight = _rect.sizeDelta.y;
+
+        _blackSurfaceBox = GetComponent<Image>();
     }
 
     public void Configure(GameObject unit, GameObject parent) {
-        float minX = rect.offsetMin.x;
-        float minY = rect.offsetMin.y;
-        float maxX = rect.offsetMax.x;
-        float maxY = rect.offsetMax.y;
+        float minX = _rect.offsetMin.x;
+        float minY = _rect.offsetMin.y;
+        float maxX = _rect.offsetMax.x;
+        float maxY = _rect.offsetMax.y;
         
         _name.text = unit.GetComponent<Unit>().Name;
         
@@ -49,17 +64,39 @@ public class UiPopUp : MonoBehaviour {
 
         transform.SetParent(parent.transform);
 
-        rect.localScale = new Vector3(1, 1, 1);
-        rect.offsetMin = new Vector2(minX, maxX);
-        rect.offsetMax = new Vector2(minY, maxY);
+        _rect.localScale = new Vector3(1, 1, 1);
+        _rect.offsetMin = new Vector2(minX, maxX);
+        _rect.offsetMax = new Vector2(minY, maxY);
 
         float boxHeight = _baseHeight + GetStringLines(_description.text) + GetStringLines(_flavour.text) * _heightPerLine;
-        rect.sizeDelta = new Vector2(_boxWidth, boxHeight);
-        rect.localPosition = new Vector3(-_boxWidth / 2, boxHeight / 2);
+        _rect.sizeDelta = new Vector2(_boxWidth, boxHeight);
+        _rect.localPosition = new Vector3(-_boxWidth / 2, boxHeight / 2);
     }
 
     private int GetStringLines(string str) {
         Regex rx = new Regex("<br>");
         return rx.Matches(str).Count + str.Length / 40;
+    }
+
+    // Controls fading
+    void Update() {
+        if (_hitUpperBound) {
+            return;
+        }
+
+        if (_timeSinceBirth > _timeStartFade) {
+            float currentAlpha = _blackSurfaceBox.color.a;
+
+            float newAlpha = currentAlpha - Time.deltaTime * _fadeSpeed;
+
+            if (newAlpha < _alphaUpperBound) {
+                newAlpha = _alphaUpperBound;
+                _hitUpperBound = true;
+            }
+
+            _blackSurfaceBox.color = new Color(_blackSurfaceBox.color.r, _blackSurfaceBox.color.g, _blackSurfaceBox.color.b, newAlpha);
+        } else {
+            _timeSinceBirth += Time.deltaTime;
+        }
     }
 }
