8074a591cfea74b8a63803750d38433e134cea3d
Wingedevil <jerroldtan@gmail.com>
2020-03-30 20:19:27 +0800

    feat: final level part 1

---
+++ Assets/Scripts/ScriptedEvents/StrikeTeamEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrikeTeamEvent : ScriptedEvent
+{
+    public Transform[] SpawnPoint;
+    public float DelayBetweenSpawns = 0.1f;
+    public GameObject[] SpawnEffects;
+    public GameObject PrefabToBeSpawned;
+
+    public override void Trigger() {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn() {
+        WaitForSeconds delay = new WaitForSeconds(DelayBetweenSpawns);
+        foreach (Transform spawn in SpawnPoint) {
+            foreach (GameObject effect in SpawnEffects) {
+                Instantiate(effect, spawn.transform.position, Quaternion.identity);
+            }
+            StartCoroutine(SpawnUnit(spawn));
+            yield return delay;
+        }
+    }
+
+    IEnumerator SpawnUnit(Transform spawn) {
+        yield return new WaitForSeconds(0.5f);
+
+        Unit bike = Instantiate(PrefabToBeSpawned, spawn.transform.position, spawn.transform.rotation).GetComponent<Unit>();
+        bike.Alignment = Alignment.Hostile;
+        bike.Start();
+        bike.Attack(TruckReferenceManager.Instance.TruckBehavior);
+    }
+}
---
+++ Assets/SinkAndDestroyEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinkAndDestroyEvent : ScriptedEvent
+{
+    public GameObject[] StuffToDestroy;
+    public float SinkTime = 10;
+    public float SinkSpeed = 1;
+
+    bool started = false;
+
+    public override void Trigger() {
+        started = true;
+        Invoke("DestroyAll", SinkTime);
+    }
+
+    private void Update() {
+        if (!started) {
+            return;
+        }
+
+        foreach (GameObject go in StuffToDestroy) {
+            var p = go.transform.position;
+            p.y -= SinkSpeed * Time.deltaTime;
+            go.transform.position = p;
+        }
+    }
+
+    void DestroyAll() {
+        foreach (GameObject go in StuffToDestroy) {
+            Destroy(go);
+        }
+        Destroy(this);
+    }
+}
