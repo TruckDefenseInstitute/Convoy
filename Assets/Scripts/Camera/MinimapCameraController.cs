a41db6f9d71a7785e402dd351e154be49e93d67c
Wingedevil <jerroldtan@gmail.com>
2020-03-30 15:40:23 +0800

    cleanup: cleanup final scene

--- Assets/Scripts/Camera/MinimapCameraController.cs
+++ Assets/Scripts/Camera/MinimapCameraController.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MinimapCameraController : MonoBehaviour {   
+public class MinimapCameraController : MonoBehaviour {
+    float _shadowDistance;
 
     public void SetMinimapCameraLocation(float x, float y, float z, float size) {
         transform.position = new Vector3(x, y, z);
-        GetComponent<Camera>().orthographicSize = size;
+        var camera = GetComponent<Camera>();
+        camera.orthographicSize = size;
     }
 
+    private void OnPreRender() {
+        _shadowDistance = QualitySettings.shadowDistance;
+        QualitySettings.shadowDistance = 0;
+    }
+
+    private void OnPostRender() {
+        QualitySettings.shadowDistance = _shadowDistance;
+    }
 }
