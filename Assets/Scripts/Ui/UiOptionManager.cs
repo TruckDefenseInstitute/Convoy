64fc36b71249fa80ba91ec86eca9f1a8b90f8e1e
Kyzure <Kyzurene@gmail.com>
2020-03-31 20:28:26 +0800

    Added option menu to pause menu to work now

---
+++ Assets/Scripts/Ui/UiOptionManager.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UiOptionManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _optionMenu = null;
+    [SerializeField]
+    private GameObject _previousMenu = null;
+
+    private List<string> _graphicsList;
+    private List<KeyValuePair<int, int>> _resolutionList;
+    private TextMeshProUGUI _graphicsText;
+    private TextMeshProUGUI _resolutionText;
+    private int _graphicsIndex = 0;
+    private int _resolutionIndex = 0;
+
+    public void Start() {
+        _graphicsText = _optionMenu.transform.GetChild(2).GetChild(4).GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
+        _resolutionText =_optionMenu.transform.GetChild(2).GetChild(5).GetChild(1).gameObject.GetComponent<TextMeshProUGUI>();
+
+        ConfigureGraphicsOption();
+        ConfigureResolutionOption();
+    }
+
+    // For Option Buttons
+    public void BackToMain() {
+        UiSoundManager.Instance.PlayButtonClickSound();
+        DisappearMenu(_optionMenu);
+        StartCoroutine(LateBackToMain());
+    }
+
+    public void ApplyOptions() {
+        QualitySettings.SetQualityLevel(_graphicsIndex);
+        KeyValuePair<int, int> res = _resolutionList[_resolutionIndex];
+        Screen.SetResolution(res.Key, res.Value, true);
+        BackToMain();
+    }
+
+    private void DisappearMenu(GameObject buttonsGameObject) {
+        foreach(Transform buttons in buttonsGameObject.transform) {
+            Animator animator = buttons.GetComponent<Animator>();
+            animator.SetBool("Disappeared", true);
+            animator.SetTrigger("Disappear");
+        }
+    }
+
+    private void ReappearMenu(GameObject buttonsGameObject) {
+        foreach(Transform buttons in buttonsGameObject.transform) {
+            Animator animator = buttons.GetComponent<Animator>();
+            animator.SetBool("Disappeared", false);
+            animator.SetTrigger("Reappear");
+        }
+    }
+
+    private void ConfigureGraphicsOption() {
+        _graphicsList = new List<string>() {
+            "Very Low",
+            "Low",
+            "Medium",
+            "High",
+            "Very High",
+            "Ultra"
+        };
+        _graphicsIndex = QualitySettings.GetQualityLevel();
+        _graphicsText.text = _graphicsList[_graphicsIndex];
+    }
+
+    private void ConfigureResolutionOption() {
+        _resolutionList = new List<KeyValuePair<int, int>>() {
+            new KeyValuePair<int, int>(1024, 576),
+            new KeyValuePair<int, int>(1280, 720),
+            new KeyValuePair<int, int>(1600, 900),
+            new KeyValuePair<int, int>(1920, 1080)
+        };
+        int width = Screen.currentResolution.width;
+        int height = Screen.currentResolution.height;
+        _resolutionIndex = _resolutionList.FindIndex(res => (res.Key == width) && (res.Value == height));
+        _resolutionText.text = width + " x " + height;
+    }
+
+    IEnumerator LateBackToMain() {
+        yield return new WaitForSecondsRealtime(1f);
+        _optionMenu.SetActive(false);
+        _previousMenu.SetActive(true);
+        ReappearMenu(_previousMenu);
+    }
+
+    // Button Spam
+    public void LeftGraphicsButton() {
+        _graphicsIndex -= 1;
+        if(_graphicsIndex < 0) {
+            _graphicsIndex = _graphicsList.Count - 1;
+        }
+        _graphicsText.text = _graphicsList[_graphicsIndex];
+    }
+
+    public void RightGraphicsButton() {
+        _graphicsIndex = (_graphicsIndex + 1) % (_graphicsList.Count);
+        _graphicsText.text = _graphicsList[_graphicsIndex];
+    }
+
+    public void LeftResolutionButton() {
+        _resolutionIndex -= 1;
+        if(_resolutionIndex < 0) {
+            _resolutionIndex = _resolutionList.Count - 1;
+        }
+        KeyValuePair<int, int> res = _resolutionList[_resolutionIndex];
+        _resolutionText.text = res.Key + " x " + res.Value;
+    }
+
+    public void RightResolutionButton() {
+        _resolutionIndex = (_resolutionIndex + 1) % (_resolutionList.Count);
+        KeyValuePair<int, int> res = _resolutionList[_resolutionIndex];
+        _resolutionText.text = res.Key + " x " + res.Value;
+    }
+
+}
